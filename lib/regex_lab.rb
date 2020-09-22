1124a027480397d78f0a8fbca728f83cf5c03420
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-22 09:48:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text_scan = text.scan(/\w+/)
+if text_scan
 end
 
 def valid_phone_number?(phone)
